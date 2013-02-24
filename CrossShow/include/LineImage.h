1f61aad370ffb03b5d3ad53c5286daa1f7367d0d
daniel.liu17 <daniel.liu17@fc9bf107-0145-0286-b814-82929a5301f6>
2013-02-24 14:40:05 +0000

    add delete of struct element in list

--- CrossShow/include/LineImage.h
+++ CrossShow/include/LineImage.h
@@ -5,7 +5,8 @@
 #include <opencv.hpp>
 #include <highgui.h>
 #include <iostream>
-#include<vector>
+#include <list>
+#include <algorithm>
 //黑白图像，黑背景+白线条
 
 
@@ -27,16 +28,37 @@ struct LINEIFO{
 	int lineNum;
 	int sum;
 };
-class CalcObj{
 
- 
+
+class CalcObj{
 public:
 	CalcObj();
 	~CalcObj();
 	void addone(int linenum,int sum);
 	LINEIFO getTheMaxElement();
 	void clearAll();
+	int listSize();
+	void removeWhereSumis(const int& value);
 private:
-	std::vector<LINEIFO> m_vect;
+
+	std::list<LINEIFO> m_list;
+	class isElementSumEqual//remove_if的函数子(必须重载perator()).此类在remove_if中可以当作函数来调用
+	{//相当于自定义一个满足一定条件的回调函数给remove_if
+	public:
+		isElementSumEqual(const int& outerParam):m_outParam(outerParam){}
+		bool operator ()(LINEIFO inerParam)
+		{
+			if (inerParam.sum==m_outParam)//所要满足的条件
+			{
+				return true;
+			}
+			else
+				return false;
+		}
+	private:
+		int m_outParam;
+	};
 };
+
+
 #endif
--- CrossShow/source/LineImage.cpp
+++ CrossShow/source/LineImage.cpp
@@ -1,6 +1,6 @@
 #include "../include/global_def.h"
 #include "../include/LineImage.h"
-int WHITE_PART=100;
+int WHITE_PART=100;//黑白图片中白色值设置为小于255的一半，所以在两图相加后，仍小于255，不会益处。
 LineImage::LineImage(CvSize size)
 {
 	m_pImage = cvCreateImage(size,IPL_DEPTH_8U,1);
@@ -36,34 +36,36 @@ void CalcObj::addone(int linenum,int sum)
 	LINEIFO info;
 	info.lineNum = linenum;
 	info.sum = sum;
-	m_vect.push_back(info);
+	m_list.push_back(info);
+
 }
 
 LINEIFO CalcObj::getTheMaxElement()
 {
-	std::vector<LINEIFO>:: iterator  it;
+	std::list<LINEIFO>:: iterator  it=m_list.begin();
 	 LINEIFO maxSum;
-	/* maxSum.sum =m_vect.begin()->lineNum;
-	for (it=m_vect.begin();it!=m_vect.end(); it++)
-	{
-		if(it->sum>maxSum.sum)
-		{
-			maxSum.sum=it->sum;
-			maxSum.lineNum = it->lineNum;
-		}
-	}*/
-	maxSum.sum = m_vect[0].sum;
-	 for (int i=0;i<m_vect.size();++i)
+	
+	maxSum.sum = it->sum;
+	 for (it;it!=m_list.end();++it)
 	 {
-		 if (m_vect[i].lineNum>maxSum.sum)
+		 if (it->sum>maxSum.sum)
 		 {
-			 maxSum.sum = m_vect[i].sum;
-			 maxSum.lineNum = m_vect[i].lineNum;
+			 maxSum.sum = it->sum;
+			 maxSum.lineNum = it->lineNum;
 		 }
 	 }
 	 return maxSum;
 }	
 void CalcObj::clearAll()
 {
-	m_vect.clear();
+	m_list.clear();
+}
+
+void CalcObj::removeWhereSumis(const int& value)
+{
+	m_list.remove_if( isElementSumEqual(value));
+}
+int CalcObj::listSize()
+{
+	return m_list.size();
 }
